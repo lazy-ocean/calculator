d7b922915b933cd5012b70c1e235eb463c108370
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2021-05-17 17:41:51 +0200

    refactor: refactor keys controls

--- src/utils/keyControls.js
+++ src/utils/keyControls.js
@@ -1,30 +1,24 @@
 /* eslint-disable no-restricted-globals */
 import OPERATIONS from "./operations";
 
+const operationsMap = {
+  ".": "punctuation",
+  "%": "special",
+  Backspace: "erase",
+  Clear: "abort",
+  Enter: "res",
+};
+
 const keyControls = (handleChange) => {
   event.preventDefault();
   const { key } = event;
 
-  if (key === "Enter") handleChange(key, "res");
-
   if (/\d/.test(key)) {
-    event.preventDefault();
     handleChange(parseInt(key, 10), "number");
   } else if (OPERATIONS.includes(key)) {
-    event.preventDefault();
     handleChange(key, "operation");
-  } else if (key === ".") {
-    event.preventDefault();
-    handleChange(key, "punctuation");
-  } else if (key === "%") {
-    event.preventDefault();
-    handleChange(key, "special");
-  } else if (key === "Backspace") {
-    event.preventDefault();
-    handleChange(key, "erase");
-  } else if (key === "Clear") {
-    event.preventDefault();
-    handleChange(key, "abort");
+  } else {
+    handleChange(key, operationsMap[key]);
   }
 };
 
