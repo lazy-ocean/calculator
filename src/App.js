4afec176b6046bb1153e999592df4db5d23214cf
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2021-05-17 16:15:51 +0200

    style: add eslint + edit code accordingly

--- src/App.js
+++ src/App.js
@@ -3,54 +3,46 @@ import React, { useEffect, useState } from "react";
 import { evaluate, format } from "mathjs";
 import classNames from "classnames";
 
+const operations = ["-", "+", "/", "*"];
+const buttons = [
+  { value: "%", role: "special" },
+  { value: "π", role: "special" },
+  { value: "e", role: "special" },
+  { value: "/", role: "operation" },
+  { value: "7", role: "number" },
+  { value: "8", role: "number" },
+  { value: "9", role: "number" },
+  { value: "x", role: "operation" },
+  { value: "4", role: "number" },
+  { value: "5", role: "number" },
+  { value: "6", role: "number" },
+  { value: "-", role: "operation" },
+  { value: "1", role: "number" },
+  { value: "2", role: "number" },
+  { value: "3", role: "number" },
+  { value: "+", role: "operation" },
+  { value: "0", role: "number", long: true },
+  { value: ".", role: "punctuation" },
+  { value: "⌫", role: "erase" },
+  { value: "C", role: "abort", long: true },
+  { value: "=", role: "res" },
+];
+
 function App() {
-  let [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
+  // eslint-disable-next-line prefer-const
   let [formula, setFormula] = useState("");
-  let [error, setError] = useState(false);
-  let [state, setState] = useState("number");
-  let [fadeOutIn, setFadeOutIn] = useState(false);
-
-  let handleKeyDown = (event) => {
-    event.preventDefault();
-    let { key } = event;
-
-    if (key === "Enter") handleChange(key, "res");
-
-    if (/\d/.test(key)) {
-      event.preventDefault();
-      handleChange(parseInt(key, 10), "number");
-    } else if (operations.includes(key)) {
-      event.preventDefault();
-      handleChange(key, "operation");
-    } else if (key === ".") {
-      event.preventDefault();
-      handleChange(key, "punctuation");
-    } else if (key === "%") {
-      event.preventDefault();
-      handleChange(key, "special");
-    } else if (key === "Backspace") {
-      event.preventDefault();
-      handleChange(key, "erase");
-    } else if (key === "Clear") {
-      event.preventDefault();
-      handleChange(key, "abort");
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyDown);
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  });
+  const [error, setError] = useState(false);
+  const [state, setState] = useState("number");
+  const [fadeOutIn, setFadeOutIn] = useState(false);
 
   const handleValue = (v) => {
-    let fr = v.toString().split(".");
+    const fr = v.toString().split(".");
     let value = v;
     if (fr[1] && fr[1].length > 6) {
       value = format(value, 6);
-      let arr = formula.split(" ");
-      let num = Number(arr[arr.length - 1]);
+      const arr = formula.split(" ");
+      const num = Number(arr[arr.length - 1]);
       arr[arr.length - 1] = format(num, 6);
       setFormula(arr.join(" "));
     }
@@ -84,10 +76,10 @@ function App() {
         }
         setState(role);
         break;
-      case "punctuation":
+      case "punctuation": {
         if (state !== "number") return;
-        let arr = formula.split(" ");
-        if (isNaN(`${arr[arr.length - 1]}${v}`)) {
+        const arr = formula.split(" ");
+        if (Number.isNaN(`${arr[arr.length - 1]}${v}`)) {
           setError("This is not a number");
         } else {
           setFormula((formula += v));
@@ -95,34 +87,33 @@ function App() {
         }
         setState(role);
         break;
+      }
       case "special":
         if (v === "%") {
-          let f = formula.split(" ");
+          const f = formula.split(" ");
           if (f.length < 2) {
-            let temp = format(evaluate(`0.01 * ${value}`), 6);
+            const temp = format(evaluate(`0.01 * ${value}`), 6);
             handleValue(temp);
             setFormula(temp);
           } else {
-            let percentage = (~~f[0] / 100) * f[2];
-            let temp = evaluate(`${f[0]}${f[1]}${percentage}`);
+            const percentage = (~~f[0] / 100) * f[2];
+            const temp = evaluate(`${f[0]}${f[1]}${percentage}`);
             handleValue(temp);
             setFormula(`${f[0]} ${f[1]} ${percentage}`);
           }
           setState(role);
         } else if (v === "π") {
-          if (isNaN(`${value}3.14159`)) {
+          if (Number.isNaN(`${value}3.14159`)) {
             setError("This is not a number");
           } else {
             setFormula((formula += "3.14159"));
             handleValue(evaluate(formula));
           }
+        } else if (Number.isNaN(`${value}2.71828`)) {
+          setError("This is not a number");
         } else {
-          if (isNaN(`${value}2.71828`)) {
-            setError("This is not a number");
-          } else {
-            setFormula((formula += "2.71828"));
-            handleValue(evaluate(formula));
-          }
+          setFormula((formula += "2.71828"));
+          handleValue(evaluate(formula));
         }
         break;
       case "res":
@@ -137,7 +128,7 @@ function App() {
         setValue("0");
         setError(false);
         break;
-      case "erase":
+      case "erase": {
         setError(false);
         let temp;
         if (
@@ -158,77 +149,76 @@ function App() {
           }
           try {
             handleValue(evaluate(temp));
-          } catch (e) {}
+          } catch (e) {
+            console.log(e);
+          }
         }
         break;
+      }
       default:
         break;
     }
   };
 
+  const handleKeyDown = (event) => {
+    event.preventDefault();
+    const { key } = event;
+
+    if (key === "Enter") handleChange(key, "res");
+
+    if (/\d/.test(key)) {
+      event.preventDefault();
+      handleChange(parseInt(key, 10), "number");
+    } else if (operations.includes(key)) {
+      event.preventDefault();
+      handleChange(key, "operation");
+    } else if (key === ".") {
+      event.preventDefault();
+      handleChange(key, "punctuation");
+    } else if (key === "%") {
+      event.preventDefault();
+      handleChange(key, "special");
+    } else if (key === "Backspace") {
+      event.preventDefault();
+      handleChange(key, "erase");
+    } else if (key === "Clear") {
+      event.preventDefault();
+      handleChange(key, "abort");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
   return (
     <div className="calc__body">
       <div className="panel">
-        <div className={classNames({ formula: true, fadeOutUp: fadeOutIn })}>
-          {formula}
-        </div>
-        <div className={classNames("result", { fadeOutUp: fadeOutIn })}>
-          {value}
-        </div>
+        <div className={classNames({ formula: true, fadeOutUp: fadeOutIn })}>{formula}</div>
+        <div className={classNames("result", { fadeOutUp: fadeOutIn })}>{value}</div>
         {error && <p className="error">{error}</p>}
       </div>
       <div className="buttons">
         {buttons.map((btn) => (
-          <Button
-            key={btn.value}
-            btn={btn}
-            onClick={(v, role) => handleChange(v, role)}
-          />
+          <Button key={btn.value} btn={btn} onClick={(v, role) => handleChange(v, role)} />
         ))}
       </div>
     </div>
   );
 }
 
-const operations = ["-", "+", "/", "*"];
-const buttons = [
-  { value: "%", role: "special" },
-  { value: "π", role: "special" },
-  { value: "e", role: "special" },
-  { value: "/", role: "operation" },
-  { value: "7", role: "number" },
-  { value: "8", role: "number" },
-  { value: "9", role: "number" },
-  { value: "x", role: "operation" },
-  { value: "4", role: "number" },
-  { value: "5", role: "number" },
-  { value: "6", role: "number" },
-  { value: "-", role: "operation" },
-  { value: "1", role: "number" },
-  { value: "2", role: "number" },
-  { value: "3", role: "number" },
-  { value: "+", role: "operation" },
-  { value: "0", role: "number", long: true },
-  { value: ".", role: "punctuation" },
-  { value: "⌫", role: "erase" },
-  { value: "C", role: "abort", long: true },
-  { value: "=", role: "res" },
-];
-/*let draft = (
-  <div className="button">
-    <p>{value}</p>
-  </div>
-);*/
-
 const Button = (prop) => {
-  let { btn, onClick } = prop;
-  let value = btn.value === "x" ? "*" : btn.value;
-  let classList = classNames(btn.role, {
+  const { btn, onClick } = prop;
+  const value = btn.value === "x" ? "*" : btn.value;
+  const classList = classNames(btn.role, {
     button: true,
     long: btn.long,
   });
   return (
-    <button className={classList} onClick={() => onClick(value, btn.role)}>
+    <button type="button" className={classList} onClick={() => onClick(value, btn.role)}>
       {btn.value}
     </button>
   );
